43d7dc3bcb3b5c9155e33ebec5e76db546044b90
Intrigon <73568756+Intrigon@users.noreply.github.com>
2021-11-17 16:57:39 +0700

    w12

---
+++ W12_VincentChristopher_40754/app/src/main/java/umn/ac/id/MainActivity.java
@@ -0,0 +1,36 @@
+package umn.ac.id;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button Halaman1,Halaman2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Halaman1 = findViewById(R.id.button1);
+        Halaman2 = findViewById(R.id.button2);
+
+        Halaman1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(MainActivity.this, DeteksiSensor.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+        Halaman2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(MainActivity.this, PembacaanDataSensor.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+    }
+
+}
